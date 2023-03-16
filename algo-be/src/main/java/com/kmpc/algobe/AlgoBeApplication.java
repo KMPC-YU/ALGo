c5158db2389435430ddc0cdde25d25d7451d1a35
munis <aaaa6658@naver.com>
2023-03-16 23:19:35 +0900

    backend-init

---
+++ algo-be/src/main/java/com/kmpc/algobe/AlgoBeApplication.java
@@ -0,0 +1,13 @@
+package com.kmpc.algobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoBeApplication.class, args);
+	}
+
+}
---
+++ algo-be/src/test/java/com/kmpc/algobe/AlgoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmpc.algobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
