61aadbfc627c413c62d9938e40084eb7ed578152
minhyuk <shiro_hacker@kakao.com>
2023-03-22 16:39:49 +0900

    fix / footer 적용 및 배경색 변경

--- algo-fe/src/App.vue
+++ algo-fe/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <Headers></Headers>
-  <div>
+  <div class="wrapper">
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Headers from "@compo/Headers.vue";
+import Footer from "@compo/Footer.vue";
 
 export default {
   components: {
     Headers,
+    Footer,
   },
 }
 </script>
 
 <style scoped>
-
+.wrapper {
+  background-color: #f1f1f2;
+}
 </style>
