3ee6ac42e05b4b93349d64955dcec18246706edb
minhyuk <shiro_hacker@kakao.com>
2023-03-22 16:38:50 +0900

    feat / footer 제작

---
+++ algo-fe/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center mt-4 py-3 border-top">
+      <p class="col-md-4 mb-0 text-muted">&copy; ALGo</p>
+
+      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      </a>
+
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">ALGo</a></li>
+        <li class="nav-item"><a href="/foods" class="nav-link px-2 text-muted">식품정보</a></li>
+        <li class="nav-item"><a href="/recipes" class="nav-link px-2 text-muted">레시피</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
