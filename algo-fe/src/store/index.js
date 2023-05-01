1f2ec10616afa9832e3a944e865207dc6633e3c8
박민혁 <shiro_hacker@kakao.com>
2023-05-01 23:04:59 +0900

    feat / store 로그인, 로그아웃 기능 구현

--- algo-fe/src/store/index.js
+++ algo-fe/src/store/index.js
@@ -1,101 +1,53 @@
-import { reactive, toRefs } from 'vue';
-import axios from 'axios';
 import { createStore } from 'vuex';
+import useAxios from '@/modules/axios.js'
+import createPersistedState from "vuex-persistedstate";
+import router from "@/router/router.js";
+
+const { axiosPost } = useAxios();
 
 export default createStore({
-    state: reactive({
-        accessToken: null,
-        refreshToken: null,
-        user: null,
-        status: '',
-    }),
+    state: {
+        isLoggedIn: false,
+    },
+    getters: {
+        isLoggedIn: (state) => state.isLoggedIn
+    },
     mutations: {
-        setAccessToken(state, accessToken) {
-            state.accessToken = accessToken;
-        },
-        setRefreshToken(state, refreshToken) {
-            state.refreshToken = refreshToken;
-        },
-        setUser(state, user) {
-            state.user = user;
-        },
-        setStatus(state, status) {
-            state.status = status;
+        setLoginStatus(state, status) {
+            state.isLoggedIn = status;
         },
     },
     actions: {
         login({ commit }, credentials) {
             return new Promise((resolve, reject) => {
-                commit('setStatus', 'loading');
-                axios.post('http://116.33.66.209:8080/api/v1/login', credentials)
-                    .then(response => {
-                        const accessToken = response.data.accessToken;
-                        const refreshToken = response.data.refreshToken;
-                        const user = response.data.user;
-                        localStorage.setItem('accessToken', accessToken);
-                        localStorage.setItem('refreshToken', refreshToken);
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-                        commit('setAccessToken', accessToken);
-                        commit('setRefreshToken', refreshToken);
-                        commit('setUser', user);
-                        commit('setStatus', 'success');
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        commit('setStatus', 'error');
-                        reject(error);
-                    });
+                axiosPost('/api/v1/login', credentials,
+                    (res) => {
+                    commit('setLoginStatus', 'true')
+                    router.push('/')
+                    resolve(res)
+                }, (err) => {
+                    console.error(err)
+                    reject(err)
+                })
             });
         },
         logout({ commit }) {
-            return new Promise((resolve) => {
-                localStorage.removeItem('accessToken');
-                localStorage.removeItem('refreshToken');
-                delete axios.defaults.headers.common['Authorization'];
-                commit('setAccessToken', null);
-                commit('setRefreshToken', null);
-                commit('setUser', null);
-                resolve();
-            });
-        },
-        refreshToken({ commit, state }) {
             return new Promise((resolve, reject) => {
-                axios.post('http://example.com/api/auth/refresh', { refreshToken: state.refreshToken })
-                    .then(response => {
-                        const accessToken = response.data.accessToken;
-                        localStorage.setItem('accessToken', accessToken);
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-                        commit('setAccessToken', accessToken);
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        localStorage.removeItem('accessToken');
-                        localStorage.removeItem('refreshToken');
-                        delete axios.defaults.headers.common['Authorization'];
-                        commit('setAccessToken', null);
-                        commit('setRefreshToken', null);
-                        commit('setUser', null);
-                        reject(error);
-                    });
+                axiosPost('/api/v1/logout',{},
+                    (res) => {
+                    commit('setLoginStatus', 'false')
+                        location.reload()
+                    resolve(res)
+                }, (err) => {
+                    console.error(err)
+                    commit('setLoginStatus', 'false')
+                    reject(err)
+                })
             });
         },
-        initialize({ commit }) {
-            const accessToken = localStorage.getItem('accessToken');
-            const refreshToken = localStorage.getItem('refreshToken');
-            if (accessToken && refreshToken) {
-                axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-                commit('setAccessToken', accessToken);
-                commit('setRefreshToken', refreshToken);
-                commit('setStatus', 'success');
-            }
-        },
     },
-    getters: {
-        status: (state) => state.status,
-        user: (state) => state.user,
-        accessToken: (state) => state.accessToken,
-        refreshToken: (state) => state.refreshToken,
-        isLoggedIn: (state) => state.accessToken !== null,
-    },
+    plugins: [createPersistedState({
+        storage: window.sessionStorage,
+    })],
 });
 
