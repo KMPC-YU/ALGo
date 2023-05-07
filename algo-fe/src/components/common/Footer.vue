dc2ae450905c5dff6a6c989e0debec37bf12f8d2
minhyuk <shiro_hacker@kakao.com>
2023-05-07 14:04:24 +0900

    refactor / Footer About 페이지 추가

--- algo-fe/src/components/common/Footer.vue
+++ algo-fe/src/components/common/Footer.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="container">
     <footer class="d-flex flex-wrap justify-content-between align-items-center mt-4 py-3 border-top">
-      <p class="col-md-4 mb-0 text-muted">&copy; ALGo</p>
-
-      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      </a>
+      <p class="col-md-4 mb-0 text-muted">&copy; 2023. ALGo</p>
 
       <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">ALGo</a></li>
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">메인화면</a></li>
         <li class="nav-item"><a href="/foods" class="nav-link px-2 text-muted">식품정보</a></li>
         <li class="nav-item"><a href="/recipes" class="nav-link px-2 text-muted">레시피</a></li>
+          <li class="nav-item"><a href="/recipes" class="nav-link px-2 text-muted">About</a></li>
       </ul>
     </footer>
   </div>
