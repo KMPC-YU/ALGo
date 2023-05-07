41d6eb9679c8b8b13d0d4d9fcb3ca52d7846d075
minhyuk <shiro_hacker@kakao.com>
2023-05-07 14:04:44 +0900

    refactor / Header 디자인 수정

--- algo-fe/src/components/common/Headers.vue
+++ algo-fe/src/components/common/Headers.vue
@@ -1,51 +1,108 @@
 <template>
-  <header class="p-3 border-bottom">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 text-dark text-decoration-none">
-         <img src="/ALGo_Logo.ico" alt="ALGo Logo" width="40"/>
-          <span class="fs-3 align-middle ms-2 me-2">ALGo</span>
-        </a>
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="/foods" class="nav-link px-2 link-dark">식품정보</a></li>
-          <li><a href="/recipes" class="nav-link px-2 link-dark">레시피</a></li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle link-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              커뮤니티
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Container wrapper -->
+        <div class="container">
+            <!-- Navbar brand -->
+            <a class="navbar-brand me-2" href="/">
+                <img src="/ALGo_Logo.ico" alt="ALGo Logo" width="40"/>
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
 
-<!--        Todo: 로그인 전-->
-        <div v-if="isLoggedIn === 'true'">
-          <button @click="logout" class="btn btn-primary me-2">로그아웃</button>
-        </div>
-        <div v-else>
-          <router-link to="/login" class="btn btn-primary me-2">로그인</router-link>
-          <router-link to="/signup" class="btn btn-primary">회원가입</router-link>
-        </div>
+            <!-- Toggle button -->
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarButtonsExample"
+                aria-controls="navbarButtonsExample"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <i class="fas fa-bars"></i>
+            </button>
 
-<!--        <div class="dropdown text-end">-->
-<!--          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">-->
-<!--            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">-->
-<!--          </a>-->
-<!--          <ul class="dropdown-menu text-small">-->
-<!--            <li><a class="dropdown-item" href="#">New project...</a></li>-->
-<!--            <li><a class="dropdown-item" href="#">Settings</a></li>-->
-<!--            <li><a class="dropdown-item" href="#">Profile</a></li>-->
-<!--            <li><hr class="dropdown-divider"></li>-->
-<!--            <li><a class="dropdown-item" href="#">Sign out</a></li>-->
-<!--          </ul>-->
-<!--        </div>-->
-      </div>
-    </div>
-  </header>
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarButtonsExample">
+                <!-- Left links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">식품정보</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">레시피</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            커뮤니티
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">공지사항</a></li>
+                            <li><a class="dropdown-item" href="#">자유게시판</a></li>
+                            <li><a class="dropdown-item" href="#">익명게시판</a></li>
+                            <li><a class="dropdown-item" href="#">지식IN</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">포인트</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- Left links -->
+
+                <div v-if="!isLoggedIn" class="d-flex align-items-center">
+                    <router-link class="btn btn-light px-3 me-2" to="/login">
+                        로그인
+                    </router-link>
+                    <router-link type="button" class="btn btn-light me-3" to="/signup">
+                        회원가입
+                    </router-link>
+                </div>
+
+                <div v-else class="d-flex align-items-center">
+                    <!-- Icon -->
+                    <a class="link-secondary me-4" href="#">
+                        <i class="fa-solid fa-comments fa-2xl"></i>
+                        <span class="position-absolute top-40 start-20 translate-middle badge rounded-pill bg-danger">
+                            99+
+                            <span class="visually-hidden">unread messages</span>
+                        </span>
+                    </a>
+
+                    <!-- 아바타 -->
+                    <div class="dropdown">
+                        <a
+                            class="link-secondary me-3 dropdown-toggle hidden-arrow"
+                            href="#"
+                            id="navbarDropdownMenuLink"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <img
+                                src="https://mblogthumb-phinf.pstatic.net/MjAxOTA0MjNfMTkz/MDAxNTU2MDE2ODQzNzY1.XIP77rvqSgGZPbY7vp_vhX_hGVWm1VBOlVaGe4ovrOYg.fav5jNf3PsvyBU2qVvZMBpJmevXZWRZAF3fa6LY1_tYg.JPEG.exia9133/g.jpg?type=w800"
+                                class="rounded-circle"
+                                height="40"
+                                alt="Black and White Portrait of a Man"
+                                loading="lazy"
+                            />
+                        </a>
+                        <ul
+                            class="dropdown-menu dropdown-menu-end"
+                            aria-labelledby="navbarDropdownMenuLink"
+                        >
+                            <li>
+                                <a class="dropdown-item" href="#">프로필</a>
+                            </li>
+                            <li>
+                                <button class="dropdown-item" @click="logout">로그아웃</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- Collapsible wrapper -->
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
 </template>
 
 <script>
@@ -53,23 +110,23 @@ import { computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
-  setup() {
-    const store = useStore()
-    const isLoggedIn = computed(() => {
-      return store.getters.isLoggedIn
-    })
+    setup() {
+        const store = useStore()
+        const isLoggedIn = computed(() => {
+            return store.getters.isLoggedIn
+        })
 
-    const logout = () => {
-      store.dispatch('logout')
-    }
+        const logout = () => {
+            store.dispatch('logout')
+        }
 
-    return {
-      isLoggedIn, logout,
+        return {
+            isLoggedIn, logout,
+        }
     }
-  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
