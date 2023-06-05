2b6a22eb2faf770e901f039a927984bc125b22ad
박민혁 <shiro_hacker@kakao.com>
2023-06-05 16:38:49 +0900

    refactor / 관리자 페이지 NavBar 숨김처리

--- algo-fe/src/App.vue
+++ algo-fe/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Headers></Headers>
+    <Headers v-if="currentRoute !== '/admin'"></Headers>
     <div>
       <router-view></router-view>
     </div>
@@ -11,11 +11,22 @@
 <script>
 import Headers from "./components/common/Headers.vue";
 import Footer from "./components/common/Footer.vue";
+import router from "@/router/router.js";
+import { computed } from 'vue'
 
 export default {
   components: {
     Headers,
     Footer,
   },
+  setup() {
+    const currentRoute = computed(() => {
+      return router.currentRoute.value.path
+    })
+
+    return {
+      currentRoute
+    }
+  }
 }
 </script>
